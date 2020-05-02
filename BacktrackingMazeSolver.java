802b1ac6c9148089af0e8ad79d199227056eea5c
SophiaM15 <54462507+SophiaM15@users.noreply.github.com>
2020-05-02 14:36:47 -0700

    Fix code

--- BacktrackingMazeSolver.java
+++ BacktrackingMazeSolver.java
@@ -6,18 +6,33 @@ public class BacktrackingMazeSolver {
      * Moves a rat from (x1,y1) to (x2,y2), filling in the cells as it goes, and
      * notifying a listener at each step.
      */
-    public boolean solve(Maze maze, Maze.MazeListener listener) {
+    public boolean solve(Maze maze, int x, int y, Maze.MazeListener listener) {
+        var start = maze.new Location(x, y);
+        var end = maze.new Location(x, y);
+        if (!start.canBeMovedTo()) {
+            throw new IllegalArgumentException("Bad start location");
+        }
+        if (!end.canBeMovedTo()) {
+            throw new IllegalArgumentException("Bad end location");
+        }
+        if (listener == null) {
+            throw new IllegalArgumentException("Listener cannot be null");
+        }
 
-        // TODO - if listener is null, throw an IllegalArgumentException
-        // saying "Listener cannot be null"
-
+        end.place(Maze.Cell.CHEESE);
         var path = new Stack<Maze.Location>();
+        var current = start;
 
         // TODO: initialize the current location to the initial rat location
 
         // Solution loop. At each step, place the rat and notify listener.
         while (true) {
+            current.place(Maze.Cell.RAT);
+            listener.mazeChanged(maze);
 
+            if (current.isAt(end)) {
+                return true;
+            }
             // TODO: Place the rat in the current cell
 
             // TODO: Notify the listener
@@ -33,18 +48,29 @@ public class BacktrackingMazeSolver {
                 current.place(Maze.Cell.PATH);
                 current = current.above();
             } else if (current.toTheRight().canBeMovedTo()) {
+                path.push(current);
+                current.place(Maze.Cell.PATH);
+                current = current.toTheRight();
                 // TODO Fill this in
             } else if (current.below().canBeMovedTo()) {
+                path.push(current);
+                current.place(Maze.Cell.PATH);
+                current = current.below();
                 // TODO Fill this in
             } else if (current.toTheLeft().canBeMovedTo()) {
+                path.push(current);
+                current.place(Maze.Cell.PATH);
+                current = current.toTheLeft();
                 // TODO Fill this in
             } else {
-
+                Location[x][y] = 0;
+                return false;
                 // TODO Fill this in ... mark this cell TRIED. If the path is
                 // empty, return false. Otherwise, back up (by popping from the
                 // path that is being built up)
 
             }
         }
+        return false;
     }
 }
